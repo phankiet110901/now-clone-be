efa15c55b0d7b0d6abb5478176a473dc451f0c85
phankiet110901 <phankiet110901@gmail.com>
2021-05-15 14:45:51 +0700

    add swagger into project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Now-clone')
+    .setDescription('The now clone api description')
+    .setVersion('1.0')
+    .addTag('admin')
+    .addTag('auth')
+    .addTag('user')
+    .addTag('store')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
